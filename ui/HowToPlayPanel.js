2ea925c8b1586002533183504cdba01f059abc51
ytech3 <ytech3@aol.com>
2025-01-17 11:39:30 -0500

    Update HowToPlayPanel.js
    Edited description to the 'HOW TO PLAY" screen

--- ui/HowToPlayPanel.js
+++ ui/HowToPlayPanel.js
@@ -5,8 +5,8 @@ export function createHowToPlayOverlay(scene) {
     overlay.innerHTML = `
         <div class="content-box">
             <h1>HOW TO PLAY</h1>
-            <p>Press SPACE or tap the screen to make the mascot fly.</p>
-            <p>Avoid obstacles and collect stars for points!</p>
+            <p>Tap the screen to make Raymond jump.</p>
+            <p>Avoid obstacles and collect opponents for points!</p>
             <div class="checkbox-container">
                 <input type="checkbox" id="agreeCheckbox">
                 <label for="agreeCheckbox">I agree to the game rules</label>
