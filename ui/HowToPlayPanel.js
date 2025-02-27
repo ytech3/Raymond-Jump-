d80ef285ea7a3e8253b9a966e5bd560cc9c88323
ytech3 <ytech3@aol.com>
2025-02-27 09:32:42 -0500

    Update HowToPlayPanel.js
    Updated T&C and Privacy Policy links

--- ui/HowToPlayPanel.js
+++ ui/HowToPlayPanel.js
@@ -27,8 +27,8 @@ export function createHowToPlayOverlay(scene) {
             <button class="start-button" disabled>Start Game</button>
             <div class="legal-links-container">
                 <button class="legal-link" onclick="window.open('https://www.mlb.com/rays/official-information/contact')">Support</button>
-                <button class="legal-link" onclick="window.open('https://www.mlb.com/official-information/terms-of-use')">MLB TOU</button>
-                <button class="legal-link" onclick="window.open('https://www.mlb.com/official-information/privacy-policy')">MLB Privacy Policy</button>
+                <button class="legal-link" onclick="window.open('https://www.mlb.com/app/ballpark/official-information/terms-of-use')">MLB TOU</button>
+                <button class="legal-link" onclick="window.open('https://www.mlb.com/app/ballpark/official-information/privacy-policy')">MLB Privacy Policy</button>
             </div>
         </div>
     `;
